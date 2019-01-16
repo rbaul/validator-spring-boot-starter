661c9183bb40c9f2950bd8cfaaab7064a48cf058
Roman Baul <Roman.Baul@ecitele.com>
2019-01-16 14:21:42 +0200

    Update javadoc of @SpringValidator

--- validator-spring-boot-starter/src/main/java/com/github/rbaul/spring/boot/validator/SpringValidator.java
+++ validator-spring-boot-starter/src/main/java/com/github/rbaul/spring/boot/validator/SpringValidator.java
@@ -17,33 +17,39 @@ public @interface SpringValidator {
 
     /**
      * Validators (default: Empty)
+     * @return Validator array
      */
     @AliasFor("validators")
     Class<? extends Validator>[] value() default {};
 
     /**
      * Validators (default: Empty)
+     * @return Validator array
      */
     @AliasFor("value")
     Class<? extends Validator>[] validators() default {};
 
     /**
      * Groups of validators (default: Empty)
+     * @return aggregation interface of validators arrays
      */
     Class<?>[] groups() default {};
 
     /**
      * Validate constraints before execution validators (default: False)
+     * @return if validate constraints before
      */
     boolean validateConstraintBefore() default false;
 
     /**
      * First not valid validator throw (default: False)
+     * @return if throw after first not valid validator
      */
     boolean firstValidatorThrow() default false;
 
     /**
      * First not valid group throw (default: False)
+     * @return if throw after first not valid group
      */
     boolean firstGroupThrow() default false;
 }
